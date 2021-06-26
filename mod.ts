edd521e7d9a1e20dc2bed89bd47d4190efa74225
tony Fang <52869633+tonyfang1992@users.noreply.github.com>
2021-06-27 01:30:12 +0800

    取得spaceX data

---
+++ mod.ts
@@ -0,0 +1,9 @@
+async function downloadLaunchData() {
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
+  });
+  const launchData = await response.json();
+  console.log(launchData);
+}
+
+await downloadLaunchData();
