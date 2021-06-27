f11cc8b79de5558a113f3e69b824bc7748ed9fda
tony Fang <52869633+tonyfang1992@users.noreply.github.com>
2021-06-27 16:15:22 +0800

    std log pratice

--- mod.ts
+++ mod.ts
@@ -1,7 +1,30 @@
+import * as log from "https://deno.land/std@0.99.0/log/mod.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    // configure default logger available via short-hand methods above.
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
+
 async function downloadLaunchData() {
+  log.info("Downloading launch data...");
+
   const response = await fetch("https://api.spacexdata.com/v3/launches", {
     method: "GET",
   });
+
+  if (!response.ok) {
+    log.warning("Problem downloading launch data.");
+    throw new Error("Launch data download failed.");
+  }
+
   const launchData = await response.json();
   console.log(launchData);
 }
